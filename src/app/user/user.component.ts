d47308c560cf3d6017f52119bc3c6d0dc0b97e35
tanmoybagani <tanmoybagani@outlook.com>
2025-02-12 00:09:00 +0530

    Implemented two-way data binding and submitted the form data.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +10,8 @@ interface User {
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
-
+  @Input({required: true}) selected!: boolean;
+  
   @Output() select = new EventEmitter();
   
   get imagePath() {
